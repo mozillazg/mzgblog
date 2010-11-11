ff3f66368505b95110763c9c14acbc4de90b20b6
opensource.mozillazg@gmail.com <opensource.mozillazg@gmail.com>
2010-11-11 16:18:38 +0800

    将 py 文件中的 tab 全部替换为 四个空格 点击 go top 时平滑滚动到页首

--- micolog/plugins/xheditor/xheditor.py
+++ micolog/plugins/xheditor/xheditor.py
@@ -3,25 +3,25 @@ import logging,os
 from model import *
 from google.appengine.api import users
 class xheditor(Plugin):
-	def __init__(self):
-		Plugin.__init__(self,__file__)
-		self.author="xuming"
-		self.authoruri="http://xuming.net"
-		self.uri="http://xuming.net"
-		self.description="xheditor."
-		self.name="xheditor plugin"
-		self.version="0.1"
-		self.register_urlzip('/xheditor/(.*)','xheditor.zip')
-		self.register_filter('editor_header',self.head)
+    def __init__(self):
+        Plugin.__init__(self,__file__)
+        self.author="xuming"
+        self.authoruri="http://xuming.net"
+        self.uri="http://xuming.net"
+        self.description="xheditor."
+        self.name="xheditor plugin"
+        self.version="0.1"
+        self.register_urlzip('/xheditor/(.*)','xheditor.zip')
+        self.register_filter('editor_header',self.head)
 
 
 
-	def head(self,content,blog=None,*arg1,**arg2):
-		if blog.language=='zh_CN':
-			js='xheditor-zh-cn.js'
-		else:
-			js='xheditor-en.js'
-		sret='''<script type="text/javascript" src="/xheditor/%s"></script>
+    def head(self,content,blog=None,*arg1,**arg2):
+        if blog.language=='zh_CN':
+            js='xheditor-zh-cn.js'
+        else:
+            js='xheditor-en.js'
+        sret='''<script type="text/javascript" src="/xheditor/%s"></script>
 <script type="text/javascript">
 $(function(){
   $("#content").xheditor(true,{
@@ -31,14 +31,14 @@ $(function(){
 });
 
 </script>'''%js
-		return sret
+        return sret
 
 
-	def get(self,page):
-		return '''<h3>xheditor Plugin </h3>
-			   <p>This is a demo for write editor plugin.</p>
-			   <h4>feature</h4>
-			   <p><ol>
-			   <li>Change editor as xheditor.</li>
-			   </ol></p>
-				'''
+    def get(self,page):
+        return '''<h3>xheditor Plugin </h3>
+               <p>This is a demo for write editor plugin.</p>
+               <h4>feature</h4>
+               <p><ol>
+               <li>Change editor as xheditor.</li>
+               </ol></p>
+                '''
